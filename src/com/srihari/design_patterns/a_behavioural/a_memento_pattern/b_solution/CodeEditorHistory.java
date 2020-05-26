39717e6cac61f60f5919cd0ff9b0a0dca83ce8c4
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-26 12:10:38 +0530

    Design Patterns - Behavioural - Memento

--- src/com/srihari/design_patterns/a_behavioural/a_memento_pattern/b_solution/CodeEditorHistory.java
+++ src/com/srihari/design_patterns/a_behavioural/a_memento_pattern/b_solution/CodeEditorHistory.java
@@ -3,7 +3,7 @@ package com.srihari.design_patterns.a_behavioural.a_memento_pattern.b_solution;
 import java.util.Stack;
 
 public class CodeEditorHistory {
-    private Stack<CodeEditorState> states = new Stack<>();
+    private final Stack<CodeEditorState> states = new Stack<>();
 
     public void push(CodeEditorState editorState) {
         states.push(editorState);
